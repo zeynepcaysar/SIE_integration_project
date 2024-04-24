5386c67e26f17a2df4bc18c348d197c80bc5cd43
Zeynep Caysar <zeynep.caysar@etu.unige.ch>
2024-04-24 15:15:59 +0000

    Update send_smtp.py

--- send_smtp.py
+++ send_smtp.py
@@ -8,18 +8,60 @@ def send_email(subject, message, from_addr, to_addr):
     msg['To'] = to_addr
 
     # Connect to FakeSMTP server
-    server = smtplib.SMTP('localhost', 25)  # Default SMTP port is 25
+    server = smtplib.SMTP('localhost', 25)
     server.send_message(msg)
     server.quit()
     print("Email sent successfully!")
 
 
+#new order mail from customer
 email_subject = "New Order Confirmation"
 email_body = """
+Hello, this is my order below,
+- address:chemin de soleil, 1223, Geneva
 - Order Date: April 16, 2024
 - Delivery Date: April 28, 2024
-- Product ID: 31
+- Product ID: 21
+- Quantity: 2
+
+- Order Date: April 16, 2024
+- Delivery Date: April 28, 2024
+- Product ID: 5
+- Quantity: 1
+
+- Order Date: April 16, 2024
+- Delivery Date: April 28, 2024
+- Product ID: 1
+- Quantity: 1
+
+Thank you,
+Best Regards,
+"""
+
+send_email(email_subject, email_body, 'customer1020@example.com', 'sales@mycompany.com')
+
+
+#reply: insufficient stocks
+#answer 1: order other available products
+'''
+email_subject = "reply: insufficient stocks"
+email_body = """
+1
+- address:chemin de soleil, 1223, Geneva
+- Order Date: April 16, 2024
+- Delivery Date: April 28, 2024
+- Product ID: 21
 - Quantity: 2
 """
 
-send_email(email_subject, email_body, 'customer6@example.com', 'sales@mycompany.com')
+'''
+#answer 2: wait for product replenishment
+'''
+email_subject = "reply: insufficient stocks"
+email_body = """
+2
+product id: 5
+quantity:2
+"""
+'''
+
